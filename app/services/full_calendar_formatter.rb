dc24776c73bcf3d3e452df12ec6d606643bfd845
Michael Ball <cycomachead@gmail.com>
2021-06-23 19:36:46 -0700

    delinting

--- app/services/full_calendar_formatter.rb
+++ app/services/full_calendar_formatter.rb
@@ -29,16 +29,16 @@ class FullCalendarFormatter
       url = Rails.application.routes.url_helpers.conference_program_proposal_path(conference.short_title, event_schedule.event.id)
 
       {
-        id:                event.guid,
-        title:             event.title,
-        start:             event_schedule.start_time_in_conference_timezone,
-        end:               event_schedule.end_time_in_conference_timezone,
-        resourceId:        event_schedule.room.guid,
-        url:               url,
-        borderColor:       event_type_color,
-        backgroundColor:   event_type_color,
-        textColor:         contrast_color(event_type_color),
-        className:         "fc-event-track-#{event.track&.short_name || 'none'}"
+        id:              event.guid,
+        title:           event.title,
+        start:           event_schedule.start_time_in_conference_timezone,
+        end:             event_schedule.end_time_in_conference_timezone,
+        resourceId:      event_schedule.room.guid,
+        url:             url,
+        borderColor:     event_type_color,
+        backgroundColor: event_type_color,
+        textColor:       contrast_color(event_type_color),
+        className:       "fc-event-track-#{event.track&.short_name || 'none'}"
       }
     end
   end
