5b7064d525a555a3d4a8b593bd8388d91a9534c4
Michael Ball <cycomachead@gmail.com>
2021-06-25 14:52:03 -0700

    Some quick schedule enhancements.
    * fix caching with the program data (hopefully)
    * not the conference timezone
    * improve clicking links to inyourtimezone
    * fix a couple page titles

--- app/assets/javascripts/osem-schedule.js
+++ app/assets/javascripts/osem-schedule.js
@@ -156,7 +156,7 @@ function eventClicked(e, element) {
 /* Links inside event-panel (to make ctrl + click work for these links):
  = link_to text, '#', onClick: 'insideLinkClicked();', 'data-url' => url
 */
-function insideLinkClicked(event){
+function insideLinkClicked(event) {
   // stops the click from propagating
   if (!event) // for IE
     var event = window.event;
@@ -164,7 +164,7 @@ function insideLinkClicked(event){
   if (event.stopPropagation) event.stopPropagation();
 
   var url = $(event.target).data('url');
-  if(event.ctrlKey)
+  if(event.ctrlKey || e.metaKey)
     window.open(url,'_blank');
   else
     window.location = url;
