3dcf7223879d0c60dc2e4bc0617ececdf20e1cce
Michael Ball <cycomachead@gmail.com>
2021-07-09 03:34:31 -0700

    Setup bullet for query auditing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -118,6 +118,13 @@ Osem::Application.configure do
       # Enable ichain test mode
       devise_config.ichain_test_mode = true
     end
+
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+    Bullet.skip_html_injection = false
   end
 
   config.active_record.verbose_query_logs = true
