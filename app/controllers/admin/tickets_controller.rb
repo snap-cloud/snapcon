52de2520669628627972414de4e5a7322a75c707
Michael Ball <cycomachead@gmail.com>
2021-07-20 17:16:35 -0700

    create a registration when giving a registration ticket

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -42,9 +42,10 @@ module Admin
       ticket_purchase = @ticket.ticket_purchases.new(gift_ticket_params)
       recipient = ticket_purchase.user
       if ticket_purchase.save
+        registration = register_user(recipient) if @ticket.registration_ticket?
         redirect_to(
           admin_conference_ticket_path(@conference.short_title, @ticket),
-          notice: "#{recipient.name} was given a #{@ticket.title} ticket."
+          notice: "#{recipient.name} was given a #{@ticket.title} ticket #{'and registered' if registration}."
         )
       else
         redirect_back(
@@ -78,10 +79,20 @@ module Admin
     end
 
     def gift_ticket_params
-      response = params.require(:ticket_purchase).permit(
-        :user_id
-      )
+      response = params.require(:ticket_purchase).permit(:user_id)
       response.merge(paid: true, amount_paid: 0, conference: @conference)
+    end
+
+    def register_user(user)
+      registration = @conference.registrations.new()
+      registration.user = user
+      if registration.save
+        MailblusterEditLeadJob.perform_later(user.id,
+          add_tags: ["#{ENV['OSEM_NAME']}-#{@conference.short_title}"]
+        )
+        return registration
+      end
+      false
     end
   end
 end
