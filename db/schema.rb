f545cfed0176824c9f005b332a05ccdf294bf016
yewchung <philipchengreen@yahoo.com>
2022-04-13 12:39:02 -0700

    removed UTC default

--- db/schema.rb
+++ db/schema.rb
@@ -605,7 +605,7 @@ ActiveRecord::Schema.define(version: 2022_03_16_220950) do
     t.string "username"
     t.boolean "is_disabled", default: false
     t.string "picture"
-    t.string "timezone", default: "UTC"
+    t.string "timezone"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
