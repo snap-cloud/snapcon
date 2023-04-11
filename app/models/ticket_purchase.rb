5a63112fe296f9885a8378269add7070e416f107
killamonis <monmohiuddin@gmail.com>
2023-04-11 16:29:02 -0700

    functionality for more variables w/ gsub

--- app/models/ticket_purchase.rb
+++ app/models/ticket_purchase.rb
@@ -118,9 +118,39 @@ def get_values(event = nil, booth = nil)
     'conference'             => conference.title,
     'ticket_quantity'        => quantity.to_s,
     'ticket_title'           => ticket.title,
-    'ticket_purchase_id'     => ticket.id.to_s
+    'ticket_purchase_id'     => ticket.id.to_s,
+    'conference_start_date'  => conference.start_date,
+    'conference_end_date'    => conference.end_date,
+    'registrationlink'       => Rails.application.routes.url_helpers.conference_conference_registration_url(
+      conference.short_title, host: ENV.fetch('OSEM_HOSTNAME', 'localhost:3000')
+    ),
+    'conference_splash_link' => Rails.application.routes.url_helpers.conference_url(
+      conference.short_title, host: ENV.fetch('OSEM_HOSTNAME', 'localhost:3000')
+    ),
+
+    'schedule_link'          => Rails.application.routes.url_helpers.conference_schedule_url(
+      conference.short_title, host: ENV.fetch('OSEM_HOSTNAME', 'localhost:3000')
+    )
   }
-  return h
+  if conference.program.cfp
+    h['cfp_start_date'] = conference.program.cfp.start_date
+    h['cfp_end_date'] = conference.program.cfp.end_date
+  else
+    h['cfp_start_date'] = 'Unknown'
+    h['cfp_end_date'] = 'Unknown'
+  end
+  if conference.venue
+    h['venue'] = conference.venue.name
+    h['venue_address'] = conference.venue.address
+  else
+    h['venue'] = 'Unknown'
+    h['venue_address'] = 'Unknown'
+  end
+  if conference.registration_period
+    h['registration_start_date'] = conference.registration_period.start_date
+    h['registration_end_date'] = conference.registration_period.end_date
+  end
+  h
 end
 
 def generate_confirmation_mail(event_template)
