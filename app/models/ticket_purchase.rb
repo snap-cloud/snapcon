be5601cbae62542e4c5c3ffd3ad076dd0394a342
killamonis <monmohiuddin@gmail.com>
2023-04-12 14:25:51 -0700

    more rubcop fixes

--- app/models/ticket_purchase.rb
+++ app/models/ticket_purchase.rb
@@ -110,65 +110,63 @@ class TicketPurchase < ApplicationRecord
       errors.add(:quantity, 'cannot be greater than one for registration tickets.')
     end
   end
-end
 
-def retrieve_values
-  h = {
-    'name'                   => user.name,
-    'conference'             => conference.title,
-    'ticket_quantity'        => quantity.to_s,
-    'ticket_title'           => ticket.title,
-    'ticket_purchase_id'     => ticket.id.to_s,
-    'conference_start_date'  => conference.start_date,
-    'conference_end_date'    => conference.end_date,
-    'registrationlink'       => Rails.application.routes.url_helpers.conference_conference_registration_url(
-      conference.short_title, host: ENV.fetch('OSEM_HOSTNAME', 'localhost:3000')
-    ),
-    'conference_splash_link' => Rails.application.routes.url_helpers.conference_url(
-      conference.short_title, host: ENV.fetch('OSEM_HOSTNAME', 'localhost:3000')
-    ),
-
-    'schedule_link'          => Rails.application.routes.url_helpers.conference_schedule_url(
-      conference.short_title, host: ENV.fetch('OSEM_HOSTNAME', 'localhost:3000')
-    )
-  }
-  if conference.program.cfp
-    h['cfp_start_date'] = conference.program.cfp.start_date
-    h['cfp_end_date'] = conference.program.cfp.end_date
-  else
-    h['cfp_start_date'] = 'Unknown'
-    h['cfp_end_date'] = 'Unknown'
-  end
-  if conference.venue
-    h['venue'] = conference.venue.name
-    h['venue_address'] = conference.venue.address
-  else
-    h['venue'] = 'Unknown'
-    h['venue_address'] = 'Unknown'
-  end
-  if conference.registration_period
-    h['registration_start_date'] = conference.registration_period.start_date
-    h['registration_end_date'] = conference.registration_period.end_date
-  end
-  h
-end
-
-def generate_confirmation_mail(event_template)
-  parse_template(event_template, retrieve_values)
-end
-
-def parse_template(text, values)
-  values.each do |key, value|
-    if value.is_a?(Date)
-      text = text.gsub "{#{key}}", value.strftime('%Y-%m-%d') if text.present?
+  def get_values
+    h = {
+      'name'                   => user.name,
+      'conference'             => conference.title,
+      'ticket_quantity'        => quantity.to_s,
+      'ticket_title'           => ticket.title,
+      'ticket_purchase_id'     => ticket.id.to_s,
+      'conference_start_date'  => conference.start_date,
+      'conference_end_date'    => conference.end_date,
+      'registrationlink'       => Rails.application.routes.url_helpers.conference_conference_registration_url(
+        conference.short_title, host: ENV.fetch('OSEM_HOSTNAME', 'localhost:3000')
+      ),
+      'conference_splash_link' => Rails.application.routes.url_helpers.conference_url(
+        conference.short_title, host: ENV.fetch('OSEM_HOSTNAME', 'localhost:3000')
+      ),
+  
+      'schedule_link'          => Rails.application.routes.url_helpers.conference_schedule_url(
+        conference.short_title, host: ENV.fetch('OSEM_HOSTNAME', 'localhost:3000')
+      )
+    }
+    if conference.program.cfp
+      h['cfp_start_date'] = conference.program.cfp.start_date
+      h['cfp_end_date'] = conference.program.cfp.end_date
     else
-      text = text.gsub "{#{key}}", value unless text.blank? || value.blank?
+      h['cfp_start_date'] = 'Unknown'
+      h['cfp_end_date'] = 'Unknown'
     end
+    if conference.venue
+      h['venue'] = conference.venue.name
+      h['venue_address'] = conference.venue.address
+    else
+      h['venue'] = 'Unknown'
+      h['venue_address'] = 'Unknown'
+    end
+    if conference.registration_period
+      h['registration_start_date'] = conference.registration_period.start_date
+      h['registration_end_date'] = conference.registration_period.end_date
+    end
+    h
   end
-  text
-end
 
-public :generate_confirmation_mail
+  def generate_confirmation_mail(event_template)
+    parse_template(event_template, get_values)
+  end
+
+  def parse_template(text, values)
+    values.each do |key, value|
+      if value.is_a?(Date)
+        text = text.gsub "{#{key}}", value.strftime('%Y-%m-%d') if text.present?
+      else
+        text = text.gsub "{#{key}}", value unless text.blank? || value.blank?
+      end
+    end
+    text
+  end
+end
 
 private
 
