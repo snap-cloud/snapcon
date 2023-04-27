36b8123d7b06ab6a36c2f8697e4ce175821a117b
Justin Pau <jupau@berkeley.com>
2023-04-26 20:50:21 -0700

    row one for rspec

--- app/controllers/admin/currency_conversions_controller.rb
+++ app/controllers/admin/currency_conversions_controller.rb
@@ -7,16 +7,7 @@ module Admin
     def index; end
 
     # GET /currency_conversions/1
-<<<<<<< HEAD
-<<<<<<< HEAD
     def show; end
-=======
-    def show
-    end
->>>>>>> 34525cbc4 (routes are working except update)
-=======
-    def show; end
->>>>>>> ac12fba08 (rspec tests for currency conversions)
 
     # GET /currency_conversions/new
     def new
@@ -24,37 +15,16 @@ module Admin
     end
 
     # GET /currency_conversions/1/edit
-<<<<<<< HEAD
-<<<<<<< HEAD
     def edit; end
-=======
-    def edit
-    end
->>>>>>> 34525cbc4 (routes are working except update)
-=======
-    def edit; end
->>>>>>> ac12fba08 (rspec tests for currency conversions)
 
     # POST /currency_conversions
     def create
       @currency_conversion = @conference.currency_conversions.new(currency_conversion_params)
 
       if @currency_conversion.save
-<<<<<<< HEAD
-<<<<<<< HEAD
         redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: 'Currency conversion was successfully created.'
       else
         flash.now[:error] = 'Creating currency conversion failed.'
-=======
-        redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: "Currency conversion was successfully created."
-      else
-        flash.now[:error] = "Creating currency conversion failed."
->>>>>>> 34525cbc4 (routes are working except update)
-=======
-        redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: 'Currency conversion was successfully created.'
-      else
-        flash.now[:error] = 'Creating currency conversion failed.'
->>>>>>> ac12fba08 (rspec tests for currency conversions)
         render :new
       end
     end
@@ -62,21 +32,9 @@ module Admin
     # PATCH/PUT /currency_conversions/1
     def update
       if @currency_conversion.update(currency_conversion_params)
-<<<<<<< HEAD
-<<<<<<< HEAD
         redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: 'Currency conversion was successfully updated.'
       else
         flash.now[:error] = 'Updating currency conversion failed.'
-=======
-        redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: "Currency conversion was successfully updated."
-      else
-        flash.now[:error] = "Updating currency conversion failed."
->>>>>>> 34525cbc4 (routes are working except update)
-=======
-        redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: 'Currency conversion was successfully updated.'
-      else
-        flash.now[:error] = 'Updating currency conversion failed.'
->>>>>>> ac12fba08 (rspec tests for currency conversions)
         render :edit
       end
     end
@@ -84,42 +42,17 @@ module Admin
     # DELETE /currency_conversions/1
     def destroy
       if @currency_conversion.destroy
-<<<<<<< HEAD
-<<<<<<< HEAD
         redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: 'Currency conversion was successfully deleted.'
       else
         redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: 'Deleting currency conversion failed.'
-=======
-        redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: "Currency conversion was successfully deleted."
-      else
-        redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: "Deleting currency conversion failed."
->>>>>>> 34525cbc4 (routes are working except update)
-=======
-        redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: 'Currency conversion was successfully deleted.'
-      else
-        redirect_to admin_conference_currency_conversions_path(@conference.short_title), notice: 'Deleting currency conversion failed.'
->>>>>>> ac12fba08 (rspec tests for currency conversions)
       end
     end
 
     private
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ac12fba08 (rspec tests for currency conversions)
     # Only allow a list of trusted parameters through.
     def currency_conversion_params
       params.require(:currency_conversion).permit(:from_currency, :to_currency, :rate)
     end
-<<<<<<< HEAD
-=======
-      # Only allow a list of trusted parameters through.
-      def currency_conversion_params
-        params.require(:currency_conversion).permit(:from_currency, :to_currency, :rate)
-      end
->>>>>>> 34525cbc4 (routes are working except update)
-=======
->>>>>>> ac12fba08 (rspec tests for currency conversions)
   end
 end
--- spec/features/currency_conversions_spec.rb
+++ spec/features/currency_conversions_spec.rb
@@ -4,11 +4,19 @@ require 'spec_helper'
 
 describe CurrencyConversion do
   let!(:conference) { create(:conference, title: 'ExampleCon') }
+<<<<<<< HEAD
   let!(:admin) { create(:admin) }
 
   context 'as an admin' do
     before do
       sign_in admin
+=======
+  let!(:organizer) { create(:organizer, resource: conference) }
+
+  context 'as a organizer' do
+    before do
+      sign_in organizer
+>>>>>>> 306aded2d (row one for rspec)
     end
 
     after do
@@ -19,21 +27,34 @@ describe CurrencyConversion do
       visit admin_conference_currency_conversions_path(conference.short_title)
       click_link 'Add Currency Conversion'
 
+<<<<<<< HEAD
       fill_in 'currency_conversion_from_currency', with: 'USD'
       fill_in 'currency_conversion_to_currency', with: 'EUR'
       fill_in 'currency_conversion_rate', with: '0.89'
 
       click_button 'Create Currency conversion'
+=======
+      fill_in 'from_currency', with: 'USD'
+      fill_in 'to_currency', with: 'EUR'
+      fill_in 'rate', with: '0.89'
+
+      click_button 'Create Currency Conversion'
+>>>>>>> 306aded2d (row one for rspec)
       page.find('#flash')
       expect(flash).to eq('Currency conversion was successfully created.')
       within('table#currency_conversions') do
         expect(page.has_content?('USD')).to be true
         expect(page.has_content?('EUR')).to be true
+<<<<<<< HEAD
         expect(page.assert_selector('tbody tr', count: 1)).to be true
+=======
+        expect(page.assert_selector('tr', count: 1)).to be true
+>>>>>>> 306aded2d (row one for rspec)
       end
     end
 
     it 'Deletes Currency Conversion', feature: true, js: true do
+<<<<<<< HEAD
       conference.currency_conversions << create(:currency_conversion)
       visit admin_conference_currency_conversions_path(conference.short_title)
       # Remove currency conversion
@@ -49,6 +70,20 @@ describe CurrencyConversion do
       within('table#currency_conversions') do
         expect(page.assert_selector('tbody tr', count: 0)).to be true
       end
+=======
+      visit admin_conference_currency_conversions_path(conference.short_title)
+
+      # Remove currency conversion
+      within('table tr:nth-of-type(1)') do
+        click_link 'Delete'
+      end
+      page.accept_alert
+      page.find('#flash')
+
+      # Validations
+      expect(flash).to eq('Difficulty level successfully deleted.')
+      expect(page.assert_selector('tr', count: 0)).to be true
+>>>>>>> 306aded2d (row one for rspec)
     end
   end
 end
