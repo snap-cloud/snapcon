7b2e833c154d7ab2e82811701fc5dd70a4171f34
Justin Pau <jupau@berkeley.com>
2023-04-28 02:43:03 -0700

    validats that no two rows in a the same conference curr conv tbl have the same from curr and to curr

--- app/models/currency_conversion.rb
+++ app/models/currency_conversion.rb
@@ -12,4 +12,5 @@
 class CurrencyConversion < ApplicationRecord
   belongs_to :conference
   validates :rate, numericality: { greater_than: 0 }
+  validates :from_currency, uniqueness: { scope: :to_currency }
 end
