68dc99fff2aefb3c7181458d137213f5e162ed05
Michael Ball <michael@mball.co>
2023-07-17 13:04:40 +0200

    Make the ticket scanning controller respect specs+new usage

--- app/controllers/admin/ticket_scannings_controller.rb
+++ app/controllers/admin/ticket_scannings_controller.rb
@@ -3,11 +3,13 @@
 module Admin
   class TicketScanningsController < Admin::BaseController
     before_action :authenticate_user!
-    # load_resource :physical_ticket, find_by: :token
+    load_resource :physical_ticket, find_by: :token
     skip_authorize_resource only: [:create]
 
     def create
-      @physical_ticket = PhysicalTicket.find_by(token: params[:physical_ticket][:token])
+      if not @physical_ticket and params[:physical_ticket]
+        @physical_ticket = PhysicalTicket.find_by(token: params[:physical_ticket][:token])
+      end
       @ticket_scanning = TicketScanning.new(physical_ticket: @physical_ticket)
       authorize! :create, @ticket_scanning
       @ticket_scanning.save
