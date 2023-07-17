9646bdff3d474e70039a1d2debe5ea6be15b235b
Michael Ball <michael@mball.co>
2023-07-17 02:25:03 +0200

    Cleanup code

--- app/controllers/admin/ticket_scannings_controller.rb
+++ app/controllers/admin/ticket_scannings_controller.rb
@@ -4,13 +4,10 @@ module Admin
   class TicketScanningsController < Admin::BaseController
     before_action :authenticate_user!
     # load_resource :physical_ticket, find_by: :token
-    # We authorize manually in these actions
     skip_authorize_resource only: [:create]
 
     def create
-      puts "CREATE CALLED #{params}"
       @physical_ticket = PhysicalTicket.find_by(token: params[:physical_ticket][:token])
-      debugger
       @ticket_scanning = TicketScanning.new(physical_ticket: @physical_ticket)
       authorize! :create, @ticket_scanning
       @ticket_scanning.save
