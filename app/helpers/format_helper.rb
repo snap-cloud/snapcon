5ee95ea74b7e5505161044f7d825bfd54ad9b4d4
Michael Ball <michael@mball.co>
2023-07-18 16:24:34 +0200

    two minor bugfixes

--- app/helpers/format_helper.rb
+++ app/helpers/format_helper.rb
@@ -111,7 +111,7 @@ module FormatHelper
   end
 
   def word_pluralize(count, singular, plural = nil)
-    if count == 1 || count =~ /^1(\.0+)?$/
+    if count == 1 || (count > 0 && count < 2)
       singular
     else
       plural || singular.pluralize
