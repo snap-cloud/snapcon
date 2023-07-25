28e751bd3cc108cf17bd40f5f2e695387cb61e32
Michael Ball <michael@mball.co>
2023-07-24 18:41:49 -0700

    WIP Cleanup registrations datatable to show price+attendance

--- app/datatables/registration_datatable.rb
+++ app/datatables/registration_datatable.rb
@@ -5,6 +5,9 @@ class RegistrationDatatable < AjaxDatatablesRails::ActiveRecord
 
   def_delegator :@view, :dom_id
   def_delegator :@view, :edit_admin_conference_registration_path
+  # def_delegator :@view, :delete_admin_conference_registration_path
+  def_delegator :@view, :admin_conference_registration_toggle_attendance_path
+
 
   def initialize(params, opts = {})
     @view = opts[:view_context]
@@ -17,6 +20,8 @@ class RegistrationDatatable < AjaxDatatablesRails::ActiveRecord
       name:                     { source: 'User.name' },
       email:                    { source: 'User.email' },
       accepted_code_of_conduct: { source: 'Registration.accepted_code_of_conduct', searchable: false },
+      attended:                 { source: 'Registration.attended', searchable: false },
+      ticket_price:             { source: 'TicketPurchase.amount_paid' },
       ticket_type:              { source: 'Ticket.title' },
       actions:                  { source: 'Registration.id', searchable: false, orderable: false }
     }
@@ -34,6 +39,14 @@ class RegistrationDatatable < AjaxDatatablesRails::ActiveRecord
     end.compact
   end
 
+  def registration_ticket(record)
+    record.user.tickets.for_registration(conference)
+  end
+
+  def registration_ticket_price(record)
+    record.user.ticket_purchases.where(ticket: registration_ticket(record)).first.amount_paid
+  end
+
   def data
     records.map do |record|
       {
@@ -42,15 +55,18 @@ class RegistrationDatatable < AjaxDatatablesRails::ActiveRecord
         roles:                    conference_role_titles(record.user),
         email:                    record.email,
         accepted_code_of_conduct: !!record.accepted_code_of_conduct, # rubocop:disable Style/DoubleNegation
-        ticket_type:              record.user.tickets.where(conference: conference).pluck(:title),
+        ticket_type:              registration_ticket(record).title,
+        ticket_price:             registration_ticket_price(record),
+        attended:                false,
         edit_url:                 edit_admin_conference_registration_path(conference, record),
+        # delete_url:               delete_admin_conference_registration_path(conference, record),
         DT_RowId:                 dom_id(record)
       }
     end
   end
 
   def get_raw_records # rubocop:disable Naming/AccessorMethodName
-    conference.registrations.includes(user: %i[roles tickets]).references(:users, :roles).distinct
+    conference.registrations.includes(user: %i[roles tickets ticket_purchases]).references(:users, :roles).distinct
   end
 
   # override upstream santitation, which converts everything to strings
