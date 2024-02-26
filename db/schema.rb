6543594282034d08b270e4c5f46087f73ba656f8
Tiffany Lam <tiffanylam14@berkeley.edu>
2024-02-26 15:25:56 -0800

    renamed event_types attribute submission_instructions to submission_template

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_012731) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_26_175634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -223,7 +223,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_012731) do
     t.integer "program_id"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
-    t.text "submission_instructions"
+    t.text "submission_template"
   end
 
   create_table "event_users", force: :cascade do |t|
