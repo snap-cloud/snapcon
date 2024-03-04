95fc5da215cf9df0409230dfd0eb900492b0de09
Owen Hu <owenhu0410@berkeley.edu>
2024-03-04 11:56:10 -0800

    Make currencies available to view

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,9 +4,14 @@ class TicketsController < ApplicationController
   before_action :authenticate_user!
   load_resource :conference, find_by: :short_title
   before_action :load_tickets
+
+  before_action :load_currency_conversions, only: :index
+
   authorize_resource :ticket, through: :conference
   authorize_resource :conference_registrations, class: Registration
   before_action :check_load_resource, only: :index
+
+  
 
   def index
     # Clear out unpaid tickets so a user can reselect registration tickets.
@@ -19,5 +24,10 @@ class TicketsController < ApplicationController
 
   def load_tickets
     @tickets = @conference.tickets.visible.order(:title)
+  end
+
+  def load_currency_conversions
+    @currency_conversions = @conference.currency_conversions
+    @currencies = @currency_conversions.map(&:to_currency).uniq
   end
 end
