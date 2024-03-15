baffd34d1f6c6b22fa0fac34f6b9c4b98ddd956d
Owen Hu <owenhu0410@berkeley.edu>
2024-03-14 20:39:38 -0700

    Migration add currency to ticket_purchases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_08_190204) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_15_025823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -544,6 +544,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_08_190204) do
     t.integer "payment_id"
     t.integer "week"
     t.float "amount_paid", default: 0.0
+    t.string "currency"
   end
 
   create_table "ticket_scannings", force: :cascade do |t|
