86c43be374482b320859171aaebcac7a21423570
Owen Hu <owenhu0410@berkeley.edu>
2024-03-30 22:57:32 -0400

    implement currency selector on tickets page

--- app/assets/javascripts/osem-tickets.js
+++ app/assets/javascripts/osem-tickets.js
@@ -1,20 +1,36 @@
 function update_price($this){
     var id = $this.data('id');
+    var selectedCurrency = $('#currency_selector').val();
+    var conversionData = window.currencyRates[selectedCurrency] || { rate: 1, symbol: '$' };
+    var conversionRate = conversionData.rate;
+
+    var originalPrice = parseFloat($('#price_'+id).data('original-price'));
+    var convertedPrice = originalPrice*conversionRate;
 
     // Calculate price for row
-    var value = $this.val();
-    var price = $('#price_' + id).text();
-    $('#total_row_' + id).text((value * price).toFixed(2));
+    $('#currency_symbol_' + id).text(conversionData.symbol);
+    $('#total_currency_symbol_' + id).text(conversionData.symbol);
+    $('#price_' + id).text(convertedPrice.toFixed(2));
+    $('#total_row_' + id).text((convertedPrice * $this.val()).toFixed(2));
 
     // Calculate total price
     var total = 0;
-    $('.total_row').each(function( index ) {
-        total += parseFloat($(this).text());
+    $('.total_row').each(function() {
+        total += parseFloat($(this).text().trim());
     });
-    $('#total_price').text(total.toFixed(2));
+    $('#total-currency-symbol').text(conversionData.symbol);
+    $('#total-price').text(total.toFixed(2));
 }
 
 $( document ).ready(function() {
+
+    window.currencyRates = {};
+    if (window.currencyMeta) {
+        window.currencyMeta.forEach(function(currencyInfo) {
+            window.currencyRates[currencyInfo.currency] = { rate: currencyInfo.rate, symbol: currencyInfo.symbol };
+        });
+    }
+
     $('.quantity').each(function() {
         update_price($(this));
     });
@@ -24,5 +40,11 @@ $( document ).ready(function() {
     });
     $(function () {
       $('[data-toggle="tooltip"]').tooltip()
+    });
+    
+    $('#currency_selector').change(function() {
+        $('.quantity').each(function() {
+            update_price($(this));
+        });
     });
 });
